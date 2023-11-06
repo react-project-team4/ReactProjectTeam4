c400209d18f2ba72517d49a9d79cb609802aaa25
simsunghwan <sunghwan1332@g.yju.ac.kr>
2023-11-06 20:16:55 +0900

    merge commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Header from "./components/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShowProduct from "./components/ShowProduct";
 
 const App = () => {
-  const [login, setlogin] = useState('false');
+  const [user, setUser] = useState("Guest");
   return (
     <Router>
-      <Header login={login} />
+      <Header login={user} />
       <Routes>
-        <Route path='/' element="" />
+        <Route path="/showProduct" element={<ShowProduct user={user} />} />
+        <Route path="/login" element="로그인페이지"></Route>
       </Routes>
     </Router>
-  )
-}
-
+  );
+};
 
 export default App;
