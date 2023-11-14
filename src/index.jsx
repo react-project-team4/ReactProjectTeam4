f27d61a62ccde751ce0766cd0d332cf7fca566bf
hipo-panda <w6238w2@gmail.com>
2023-11-14 20:29:28 +0900

    ✨ feat : Add Login, Register

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "./front-end/css/Register.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
