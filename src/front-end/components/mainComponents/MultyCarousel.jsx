9ecebd79b70741ec710907e5a1f215b3ff74bbf0
Ydaehan <daehan821@naver.com>
2023-11-29 20:40:19 +0900

    merge 로그아웃시 로그인 창으로 이동

--- src/front-end/components/mainComponents/MultyCarousel.jsx
+++ src/front-end/components/mainComponents/MultyCarousel.jsx
@@ -14,48 +14,61 @@ const MultyCarousel = (props) => {
 
   return (
     <div className="p-4">
-      <h3>{category}</h3>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className={styles.swiper}
-      >
-        {data.map((item, index) => (
-          <SwiperSlide key={index} className={styles.swiperSlide}>
-            <div>
-              <Link
-                key={item.id}
-                to={`/showProduct`}
-                state={{ item }}
-                style={{ textDecoration: "none" }}
-              >
-                <div className="card mx-2 my-2 " style={{ width: "18rem" }}>
-                  <img
-                    className="card-img-top"
-                    style={{ height: "200px" }}
-                    src={item.image}
-                    alt="Card image cap"
-                  />
-                  <div
-                    className="card-body text-white bg-primary rounded-lg"
-                    style={{ opacity: "0.7" }}
+      {data.length === 0 ? (
+        <>
+          <h3>{category}</h3>
+          <h1 className="d-flex justify-content-center align-items-center p-5">
+            현재 판매중인 상품이 존재하지 않습니다.
+          </h1>
+        </>
+      ) : (
+        <>
+          <h3>{category}</h3>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className={styles.swiper}
+          >
+            {data.map((item, index) => (
+              <SwiperSlide key={index} className={styles.swiperSlide}>
+                <div>
+                  <Link
+                    key={item.id}
+                    to={`/showProduct`}
+                    state={{ item }}
+                    style={{ textDecoration: "none" }}
                   >
-                    <p className="card-text text-decoration-none">
-                      {item.name}
-                    </p>
-                    <h6 className="text-black">가격: {item.price}</h6>
-                    <h6 className="text-black">배송비: {item.shippingFee}</h6>
-                  </div>
+                    <div className="card mx-2 my-2 " style={{ width: "18rem" }}>
+                      <img
+                        className="card-img-top"
+                        style={{ height: "200px" }}
+                        src={item.image}
+                        alt="Card image cap"
+                      />
+                      <div
+                        className="card-body text-white bg-primary rounded-lg"
+                        style={{ opacity: "0.7" }}
+                      >
+                        <p className="card-text text-decoration-none">
+                          {item.name}
+                        </p>
+                        <h6 className="text-black">가격: {item.price}</h6>
+                        <h6 className="text-black">
+                          배송비: {item.shippingFee}
+                        </h6>
+                      </div>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
     </div>
   );
 };
