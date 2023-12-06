6b8d74e841232295098314f01c0012b4d811fe18
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-12-06 14:26:50 +0900

    header

--- src/front-end/components/Header.jsx
+++ src/front-end/components/Header.jsx
@@ -1,6 +1,215 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { Row, Col, Navbar, Container, Nav } from "react-bootstrap";
+// import styles from "../css/header.module.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { Link } from "react-router-dom";
+// import projectMark from "../imgs/fukuokaMark.png";
+// import {
+//   faUtensils,
+//   faPlane,
+//   faAddressCard,
+//   faSignInAlt,
+//   faUserPlus,
+//   faUserCircle,
+//   faShoppingBasket, 
+//   faListAlt,
+//   faSignOut,
+//   faSignOutAlt,
+//   faUsers,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// const Header = (props) => {
+//   const { user, setUser } = props;
+//   const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 객체를사용
+//   // 로그 아웃 함수
+//   const handleLogout = () => {
+//     localStorage.clear();
+//     setUser("Guest");
+//     // 로그아웃시 로그인 페이지 이동
+//     navigate("/login"); // 로그아웃 성공시 로그인 경로로 가게 설정
+//   };
+
+//   return (
+//     <Container>
+//       <Row className={styles.row}>
+//         <Col>
+//           <Navbar className="h-8" data-bs-theme="light">
+//             <Container>
+//               <Navbar.Brand className="navbar-brand" as={Link} to="/">
+//                 <img
+//                   alt="main"
+//                   src={projectMark}
+//                   width="50"
+//                   height="30"
+//                   className="d-inline-block align-top"
+//                 />{" "}
+//                 Japang
+//               </Navbar.Brand>
+
+//               <Nav
+//                 id="nav"
+//                 className="fs-4 justify-content-between"
+//                 style={{ width: "600px" }}
+//               >
+//                 <Nav.Item>
+//                   <Nav.Link
+//                     className={styles.link}
+//                     as={Link}
+//                     to="/productList?category=Food"
+//                   >
+//                     <FontAwesomeIcon
+//                       style={{ marginRight: "6px" }}
+//                       icon={faUtensils}
+//                     />
+//                     식품
+//                   </Nav.Link>
+//                 </Nav.Item>
+//                 <Nav.Item>
+//                   <Nav.Link
+//                     className={styles.link}
+//                     as={Link}
+//                     to="/productList?category=Travel"
+//                   >
+//                     <FontAwesomeIcon
+//                       style={{ marginRight: "6px" }}
+//                       icon={faPlane}
+//                     />
+//                     현지학기제
+//                   </Nav.Link>
+//                 </Nav.Item>
+//                 <Nav.Item>
+//                   <Nav.Link className={styles.link} as={Link} to="/">
+//                     <FontAwesomeIcon
+//                       style={{ marginRight: "6px" }}
+//                       icon={faAddressCard}
+//                     />
+//                     조원소개
+//                   </Nav.Link>
+//                 </Nav.Item>
+//               </Nav>
+//               <Nav className="d-flex ">
+//                 {user === "Guest" ? (
+//                   // 게스트
+//                   <>
+//                     <Nav.Link className={styles.link} as={Link} to="/Login">
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faSignInAlt}
+//                       />
+//                       로그인
+//                     </Nav.Link>
+//                     <Nav.Link className={styles.link} as={Link} to="/Register">
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faUserPlus}
+//                       />
+//                       회원가입
+//                     </Nav.Link>
+//                   </>
+//                 ) : user === "Buyer" ? (
+//                   // 구매자
+//                   <>
+//                     <Nav.Link className={styles.link} as={Link} to="/MyPage">
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faUserCircle}
+//                       />
+//                       프로필
+//                     </Nav.Link>
+//                     <Nav.Link className={styles.link} as={Link} to="/MyCart">
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faShoppingBasket}
+//                       />
+//                       장바구니
+//                     </Nav.Link>
+
+//                     <Nav.Link className={styles.link} onClick={handleLogout}>
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faSignOutAlt}
+//                       />
+//                       로그아웃
+//                     </Nav.Link>
+//                   </>
+//                 ) : user === "Seller" ? (
+//                   // 판매자
+//                   <>
+//                     <Nav.Link
+//                       className={styles.link}
+//                       as={Link}
+//                       to="/MyProduct"
+//                     >
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faListAlt}
+//                       />
+//                       판매 상품 리스트
+//                     </Nav.Link>
+//                     <Nav.Link>
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faUserCircle}
+//                       />
+//                       프로필
+//                     </Nav.Link>
+//                     <Nav.Link className={styles.link} onClick={handleLogout}>
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faSignOutAlt}
+//                       />
+//                       로그아웃
+//                     </Nav.Link>
+//                   </>
+//                 ) : (
+//                   // 어드민
+//                   <>
+//                     <Nav.Link
+//                       className={styles.link}
+//                       as={Link}
+//                       to="/ManageBuyer"
+//                     >
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faUsers}
+//                       />
+//                       구매자 관리
+//                     </Nav.Link>
+//                     <Nav.Link
+//                       className={styles.link}
+//                       as={Link}
+//                       to="/ManageSeller"
+//                     >
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faUsers}
+//                       />
+//                       판매자 관리
+//                     </Nav.Link>
+//                     <Nav.Link className={styles.link} onClick={handleLogout}>
+//                       <FontAwesomeIcon
+//                         style={{ marginRight: "6px" }}
+//                         icon={faSignOutAlt}
+                      
+//                       />
+//                       로그아웃
+//                     </Nav.Link>
+//                   </>
+//                 )}
+//               </Nav>
+//             </Container>
+//           </Navbar>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default Header;
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Row, Col, Navbar, Container, Nav } from "react-bootstrap";
+import { Row, Col, Navbar, Container, Nav, Dropdown } from "react-bootstrap";
 import styles from "../css/header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
@@ -21,13 +230,13 @@ import {
 
 const Header = (props) => {
   const { user, setUser } = props;
-  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 객체를사용
-  // 로그 아웃 함수
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 객체를 사용
+
   const handleLogout = () => {
     localStorage.clear();
     setUser("Guest");
     // 로그아웃시 로그인 페이지 이동
-    navigate("/login"); // 로그아웃 성공시 로그인 경로로 가게 설정
+    navigate("/login"); // 로그아웃 성공시 로그인 경로로 이동
   };
 
   return (
@@ -110,91 +319,86 @@ const Header = (props) => {
                 ) : user === "Buyer" ? (
                   // 구매자
                   <>
-                    <Nav.Link className={styles.link} as={Link} to="/MyPage">
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faUserCircle}
-                      />
-                      프로필
-                    </Nav.Link>
-                    <Nav.Link className={styles.link} as={Link} to="/MyCart">
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faShoppingBasket}
-                      />
-                      장바구니
-                    </Nav.Link>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        <FontAwesomeIcon
+                          style={{ marginRight: "6px" }}
+                          icon={faUserCircle}
+                        />
+                        {user}
+                      </Dropdown.Toggle>
 
-                    <Nav.Link className={styles.link} onClick={handleLogout}>
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faSignOutAlt}
-                      />
-                      로그아웃
-                    </Nav.Link>
+                      <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to="/MyPage">
+                          프로필
+                        </Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/MyCart">
+                          장바구니
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={handleLogout}>
+                          <FontAwesomeIcon
+                            style={{ marginRight: "6px" }}
+                            icon={faSignOutAlt}
+                          />
+                          로그아웃
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
                   </>
                 ) : user === "Seller" ? (
                   // 판매자
                   <>
-                    <Nav.Link
-                      className={styles.link}
-                      as={Link}
-                      to="/MyProduct"
-                    >
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faListAlt}
-                      />
-                      판매 상품 리스트
-                    </Nav.Link>
-                    <Nav.Link>
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faUserCircle}
-                      />
-                      프로필
-                    </Nav.Link>
-                    <Nav.Link className={styles.link} onClick={handleLogout}>
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faSignOutAlt}
-                      />
-                      로그아웃
-                    </Nav.Link>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        <FontAwesomeIcon
+                          style={{ marginRight: "6px" }}
+                          icon={faUserCircle}
+                        />
+                        {user}
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to="/MyProduct">
+                          판매 상품 리스트
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={handleLogout}>
+                          <FontAwesomeIcon
+                            style={{ marginRight: "6px" }}
+                            icon={faSignOutAlt}
+                          />
+                          로그아웃
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
                   </>
                 ) : (
                   // 어드민
                   <>
-                    <Nav.Link
-                      className={styles.link}
-                      as={Link}
-                      to="/ManageBuyer"
-                    >
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faUsers}
-                      />
-                      구매자 관리
-                    </Nav.Link>
-                    <Nav.Link
-                      className={styles.link}
-                      as={Link}
-                      to="/ManageSeller"
-                    >
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faUsers}
-                      />
-                      판매자 관리
-                    </Nav.Link>
-                    <Nav.Link className={styles.link} onClick={handleLogout}>
-                      <FontAwesomeIcon
-                        style={{ marginRight: "6px" }}
-                        icon={faSignOutAlt}
-                      
-                      />
-                      로그아웃
-                    </Nav.Link>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        <FontAwesomeIcon
+                          style={{ marginRight: "6px" }}
+                          icon={faUsers}
+                        />
+                        {user}
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to="/ManageBuyer">
+                          구매자 관리
+                        </Dropdown.Item>
+                        <Dropdown.Item as={Link} to="/ManageSeller">
+                          판매자 관리
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={handleLogout}>
+                          <FontAwesomeIcon
+                            style={{ marginRight: "6px" }}
+                            icon={faSignOutAlt}
+                          />
+                          로그아웃
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
                   </>
                 )}
               </Nav>
